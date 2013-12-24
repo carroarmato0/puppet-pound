00bb69006d630200ccdd74e880374398072462f9
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2013-12-24 14:30:23 +0100

    Correct usage of http_verbs and document them
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,10 @@
+#
+# http_verbs			1 (default) accept only standard HTTP requests (GET, POST, HEAD).
+#						2 additionally allow extended HTTP requests (PUT, DELETE).
+#						3 additionally allow standard WebDAV verbs (LOCK, UNLOCK, PROPFIND, PROPPATCH, SEARCH, MKCOL, MOVE, COPY, OPTIONS, TRACE, MKACTIVITY, CHECKOUT, MERGE, REPORT).
+#						4 additionally allow MS extensions WebDAV verbs (SUBSCRIBE, UNSUBSCRIBE, NOTIFY, BPROPFIND, BPROPPATCH, POLL, BMOVE, BCOPY, BDELETE, CONNECT).
+#						5 additionally allow MS RPC extensions verbs (RPC_IN_DATA, RPC_OUT_DATA).
+#
 class pound::params {
 
   $conf        		= '/etc/pound/pound.cfg'
@@ -8,6 +15,6 @@ class pound::params {
   $socket      		= '/var/run/pound/poundctl.socket'
   $ssl_acceleration = false
   $ssl_engine  		= '<hw>'
-  $http_verbs		= 'GET, POST, HEAD, PUT, DELETE'
+  $http_verbs		= 2
 
 }
